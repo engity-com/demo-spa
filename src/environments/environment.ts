bfb14be1b6473ed0538836d7d4ec9e75b434ce6b
Gregor Noczinski <gnoczinski@engity.com>
2023-04-04 14:46:17 +0200

    Added deployment to different stages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,14 @@
 export const environment = {
     production: false,
-    stsAuthority: 'http://local.engity.green:8987/sub/v1/',
-    clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664b',
     clientRoot: 'http://localhost:4200/',
-    apiRoot: 'http://local.engity.green:8987/sub/v1/',
+    authorities: {
+        '': {
+            stsAuthority: 'http://local.engity.green:8987/v1/',
+            clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664b',
+        },
+        'magic-link': {
+            stsAuthority: 'http://local.engity.green:8987/magic-link/v1/',
+            clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664c',
+        },
+    },
 };
