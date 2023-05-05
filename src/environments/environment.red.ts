84bcdb284f93ff7aba36cc58dee43124b2edfdfb
Gregor Noczinski <gnoczinski@engity.com>
2023-05-05 10:42:34 +0200

    Adjusted environment settings

--- src/environments/environment.red.ts
+++ src/environments/environment.red.ts
@@ -6,5 +6,15 @@ export const environment = {
             stsAuthority: 'https://id.demo.engity.app/v1/',
             clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664b',
         },
+        magicLink: {
+            subPath: 'magic-link',
+            stsAuthority: 'https://id.demo.engity.app/magic-link/v1/',
+            clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664c',
+        },
+        username: {
+            subPath: 'username',
+            stsAuthority: 'https://id.demo.engity.app/username/v1/',
+            clientId: '12f95feb-9e3e-11ec-93d7-f02f741c664d',
+        },
     },
 };
